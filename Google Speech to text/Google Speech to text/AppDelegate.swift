fa459a43770d475f3bb05d33d5d0f7b66c3cccd4
Satendra Touch <satendradagar@gmail.com>
2018-12-22 13:16:59 +0530

    initial project commit

---
+++ Google Speech to text/Google Speech to text/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Google Speech to text
+//
+//  Created by Satendra Singh on 22/12/18.
+//  Copyright © 2018 Corebits Software solutions Pvt Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Google Speech to text/Google Speech to text/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Google Speech to text
+//
+//  Created by Satendra Singh on 22/12/18.
+//  Copyright © 2018 Corebits Software solutions Pvt Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
